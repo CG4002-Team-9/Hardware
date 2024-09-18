1af38068746dc7c678e570dccd856a67121aa6f3
jared-ee <88608796+jared-ee@users.noreply.github.com>
2024-09-18 14:37:56 +0800

    Update vest.ino

--- vest.ino
+++ vest.ino
@@ -8,7 +8,7 @@
 const int IR_EMISSION_FREQ = 38000; // 38kHz is common for IR emitter
 
 // Buzzer queue variables
-const int QUEUE_SIZE = 2; // Buzzer queue size
+const int QUEUE_SIZE = 3; // Buzzer queue size
 
 struct BuzzerSound {
   int frequency;
@@ -20,8 +20,8 @@ int queueStart = 0;
 int queueEnd = 0;
 unsigned long lastBuzzerTime = 0;  // Time of the last buzzer sound
 
-// Variables for server data
-int audio_num = 0;  // Indicate which sound byte to play
+int hp = 100;
+int sp = 30;
 
 // Initialize IRSend object
 IRsend irsend;
@@ -36,7 +36,8 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
 
   // Initialize IR emitter
-  irsend.begin(IR_EMITTER_PIN);  // Initialize the IR emitter
+  irsend.begin(IR_EMITTER_PIN);
+
 }
 
 void loop() {
@@ -45,14 +46,50 @@ void loop() {
     //PlayerState playerState;
     //Serial.readBytes((char*)&playerState, sizeof(PlayerState));
 
-    // Extract audio file index from the struct
-    //audio_num = playerState.audio;
-
-    // Play the buzzer sound corresponding to audio_num
-    //playBuzzer(audio_num);
+    // refresh hp and sp on board with latest values
+    //int prev_sp = sp;
+    //int prev_hp = hp;
+    //hp = playerState.hp;
+    //sp = playerState.sp;
+    //if (hp > prev_hp) {
+      //playReviveBuzzer();
+    //} else if (sp < prev_sp) {
+      // player got hit while shielding
+      //playShieldBuzzer(sp);
+    //} else if (hp < prev_hp) {
+      // player hit while not shielding
+      //playHealthBuzzer(hp);
+    //}
   //}
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n'); // Read the incoming command
 
-  irsend.sendNEC(0x00, 0x11, 0);
+    int prev_sp = sp;
+    int prev_hp = hp;
+    // Process the serial command
+    if (command == "send shielded hit") {
+      sp = 25;
+    }
+    else if (command == "send unshielded hit") {
+      hp = 95;
+    }
+    else if (command == "revive") {
+      hp = 100;
+      sp = 30;
+    }
+    
+    if (hp > prev_hp) {
+      playReviveBuzzer();
+    } else if (sp < prev_sp) {
+      // player got hit while shielding
+      playShieldBuzzer(sp);
+    } else if (hp < prev_hp) {
+      // player hit while not shielding
+      playHealthBuzzer(hp);
+    }
+  }
+
+  irsend.sendNEC(0x00, 0x59, 0);
 
   // Buzzer playing routine using queue
   if (millis() - lastBuzzerTime > 500) {
@@ -78,150 +115,18 @@ void addToBuzzerQueue(int frequency, int duration) {
   }
 }
 
-void playBuzzer(int audio_num) {
-  // case 1-20 when player is not shielded. case 21-40 when player is shielded.
-  switch (audio_num) {
-    case 1:  // hp > 95%
-      addToBuzzerQueue(5000, 200);
-      break;
-    case 2:  // 90-95% hp
-      addToBuzzerQueue(4850, 200);
-      break;
-    case 3:  // 85-90% hp
-      addToBuzzerQueue(4700, 200);
-      break;
-    case 4:  // 80-85% hp
-      addToBuzzerQueue(4550, 200);
-      break;
-    case 5:  // 75-80% hp
-      addToBuzzerQueue(4400, 200);
-      break;
-    case 6:  // 70-75% hp
-      addToBuzzerQueue(4250, 200);
-      break;
-    case 7:  // 65-70% hp
-      addToBuzzerQueue(4100, 200);
-      break;
-    case 8:  // 60-65% hp
-      addToBuzzerQueue(3950, 200);
-      break;
-    case 9:  // 55-60% hp
-      addToBuzzerQueue(3800, 200);
-      break;
-    case 10: // 50-55% hp
-      addToBuzzerQueue(3650, 200);
-      break;
-    case 11: // 45-50% hp
-      addToBuzzerQueue(3500, 200);
-      break;
-    case 12: // 40-45% hp
-      addToBuzzerQueue(3350, 200);
-      break;
-    case 13: // 35-40% hp
-      addToBuzzerQueue(3200, 200);
-      break;
-    case 14: // 30-35% hp
-      addToBuzzerQueue(3050, 200);
-      break;
-    case 15: // 25-30% hp
-      addToBuzzerQueue(2900, 200);
-      break;
-    case 16: // 20-25% hp
-      addToBuzzerQueue(2750, 200);
-      break;
-    case 17: // 15-20% hp
-      addToBuzzerQueue(2600, 200);
-      break;
-    case 18: // 10-15% hp
-      addToBuzzerQueue(2450, 200);
-      break;
-    case 19: // 5-10% hp
-      addToBuzzerQueue(2300, 200);
-      break;
-    case 20: // 0-5% hp
-      addToBuzzerQueue(2150, 200);
-      break;
-    case 21: // sp > 95%
-      addToBuzzerQueue(5000, 200);
-      addToBuzzerQueue(5000, 200);
-      break;
-    case 22: // 90-95% sp
-      addToBuzzerQueue(4850, 200);
-      addToBuzzerQueue(4850, 200);
-      break;
-    case 23: // 85-90% sp
-      addToBuzzerQueue(4700, 200);
-      addToBuzzerQueue(4700, 200);
-      break;
-    case 24: // 80-85% sp
-      addToBuzzerQueue(4550, 200);
-      addToBuzzerQueue(4550, 200);
-      break;
-    case 25: // 75-80% sp
-      addToBuzzerQueue(4400, 200);
-      addToBuzzerQueue(4400, 200);
-      break;
-    case 26: // 70-75% sp
-      addToBuzzerQueue(4250, 200);
-      addToBuzzerQueue(4250, 200);
-      break;
-    case 27: // 65-70% sp
-      addToBuzzerQueue(4100, 200);
-      addToBuzzerQueue(4100, 200);
-      break;
-    case 28: // 60-65% sp
-      addToBuzzerQueue(3950, 200);
-      addToBuzzerQueue(3950, 200);
-      break;
-    case 29: // 55-60% sp
-      addToBuzzerQueue(3800, 200);
-      addToBuzzerQueue(3800, 200);
-      break;
-    case 30: // 50-55% sp
-      addToBuzzerQueue(3650, 200);
-      addToBuzzerQueue(3650, 200);
-      break;
-    case 31: // 45-50% sp
-      addToBuzzerQueue(3500, 200);
-      addToBuzzerQueue(3500, 200);
-      break;
-    case 32: // 40-45% sp
-      addToBuzzerQueue(3350, 200);
-      addToBuzzerQueue(3350, 200);
-      break;
-    case 33: // 35-40% sp
-      addToBuzzerQueue(3200, 200);
-      addToBuzzerQueue(3200, 200);
-      break;
-    case 34: // 30-35% sp
-      addToBuzzerQueue(3050, 200);
-      addToBuzzerQueue(3050, 200);
-      break;
-    case 35: // 25-30% sp
-      addToBuzzerQueue(2900, 200);
-      addToBuzzerQueue(2900, 200);
-      break;
-    case 36: // 20-25% sp
-      addToBuzzerQueue(2750, 200);
-      addToBuzzerQueue(2750, 200);
-      break;
-    case 37: // 15-20% sp
-      addToBuzzerQueue(2600, 200);
-      addToBuzzerQueue(2600, 200);
-      break;
-    case 38: // 10-15% sp
-      addToBuzzerQueue(2450, 200);
-      addToBuzzerQueue(2450, 200);
-      break;
-    case 39: // 5-10% sp
-      addToBuzzerQueue(2300, 200);
-      addToBuzzerQueue(2300, 200);
-      break;
-    case 40: // 0-5% sp
-      addToBuzzerQueue(2150, 200);
-      addToBuzzerQueue(2150, 200);
-      break;
-    default:
-      break;
-  }
+void playHealthBuzzer(int hp) {
+  int freq = 2000 + hp * 30; //scale sound to remaining hp
+  addToBuzzerQueue(freq, 200);
+}
+
+void playShieldBuzzer(int sp) {
+  int freq = 2000 + sp * 30; //scale sound to remaining sp
+  addToBuzzerQueue(freq, 200);
+  addToBuzzerQueue(freq, 200);
+}
+
+void playReviveBuzzer() {
+  addToBuzzerQueue(2000,200);
+  addToBuzzerQueue(5000,200);
 }
